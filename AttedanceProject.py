38cc5b03292f1b5724e1cbc42db035ee253198d4
Ddimitrako <dimidimitrakopoulos1@gmail.com>
2020-12-28 19:34:07 +0200

    code changes

--- AttedanceProject.py
+++ AttedanceProject.py
@@ -1,84 +1,3 @@
-'''import cv2
-import numpy as np
-import face_recognition
-import os
-from datetime import datetime
-
-path = 'Images'
-images = []
-classNames = []
-myList = os.listdir(path)
-print(myList)
-for cl in myList:
-    curImg = cv2.imread(f'{path}/{cl}')
-    images.append(curImg)
-    classNames.append(os.path.splitext(cl)[0]) #print only Bill gates from Bill gates.jpg
-print(classNames)
-
-def findEncodings(images):
-    encodeList = []
-    for img in images:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # convert image to RGB
-        encode = face_recognition.face_encodings(img)[0] #find the encodings
-        encodeList.append(encode)
-    return encodeList
-
-
-def markAttendance(name):
-    with open('Attendance.csv', 'r+') as f:
-        myDataList = f.readlines()
-        nameList = []
-        for line in myDataList:
-            entry = line.split(',')
-            nameList.append(entry[0])
-        if name not in nameList:
-                now = datetime.now()
-                dtString = now.strftime('Date:%D Time:%H:%M:%S')
-                f.writelines(f'\n{name},{dtString}')
-
-#### FOR CAPTURING SCREEN RATHER THAN WEBCAM
-# def captureScreen(bbox=(300,300,690+300,530+300)):
-#     capScr = np.array(ImageGrab.grab(bbox))
-#     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
-#     return capScr
-
-encodeListKnown = findEncodings(images)
-print(f'Number of known person: {len(encodeListKnown)}')
-print('Encoding Complete')
-
-cap = cv2.VideoCapture(0)
-
-while True: #while loop to get each frame
-    success, img = cap.read()
-    # img = captureScreen()
-    imgSmall = cv2.resize(img, (0, 0), None, 0.25, 0.25) #makes original image smaller by 75% for faster results
-    imgSmall = cv2.cvtColor(imgSmall, cv2.COLOR_BGR2RGB)
-
-    facesCurFrame = face_recognition.face_locations(imgSmall)
-    encodesCurFrame = face_recognition.face_encodings(imgSmall, facesCurFrame)
-
-    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        print(faceDis) # the lowest faceDis parameter is the face in the camera matched to the known face
-        matchIndex = np.argmin(faceDis)
-
-        if faceDis[matchIndex] < 0.50:
-            name = classNames[matchIndex].upper()
-            markAttendance(name)
-        else:
-            name = 'Unknown'
-        print(name)
-        y1, x2, y2, x1 = faceLoc
-        y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-        cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-        #the code below is replaced with the above to recognice unkown faces
-
-    cv2.imshow('Webcam', img)
-    cv2.waitKey(1)'''
-
 import cv2
 import numpy as np
 import face_recognition
@@ -87,16 +6,39 @@ from datetime import datetime
 from time import perf_counter
 import time
 from flask import Flask, render_template, Response, request
-
-path = 'C:/Users/ddimitrakopoulos/Documents/MEGA/Python/FaceRecognition-Streaming/Face recognition V1/FaceRecognitionProject V1/Images'
+import requests
+'''try:
+    url='http://192.168.2.5:22500' 
+    pass
+    
+    Up=requests.get('http://192.168.2.5:22500/decoder_control.cgi?loginuse=admin&loginpas=gigirivas&onestep=1&command=0')    
+    time.sleep(2)
+    Down=requests.get('http://192.168.2.5:22500/decoder_control.cgi?loginuse=admin&loginpas=gigirivas&onestep=1&command=2')
+    time.sleep(2)
+    Left=requests.get('http://192.168.2.5:22500/decoder_control.cgi?loginuse=admin&loginpas=gigirivas&onestep=1&command=4')
+    time.sleep(2)
+    Right=requests.get('http://192.168.2.5:22500/decoder_control.cgi?loginuse=admin&loginpas=gigirivas&onestep=1&command=6') 
+    print(Up.status_code, Up.reason)
+except:
+    print('exception')
+    pass
+    '''
+#path = 'C:/Users/ddimitrakopoulos/Documents/MEGA/Python/FaceRecognition-Streaming/Face recognition V1/FaceRecognitionProject V1/Images'
 #path='C:/Users/dimitris/Documents/MEGA/Python/FaceRecognition-Streaming/Face recognition V1/FaceRecognitionProject V1/Images'
-# path = '/home/pi/Desktop/Tornado-mjpeg-streamer-python-master/Images' # path gia raspberry
+path = '/home/pi/Desktop/Tornado-mjpeg-streamer-python-master/Images' # path gia raspberry
 images = []
 classNames = []
 myList = os.listdir(path)
 print(myList)
 global name
+flags=[]
+startTime=[]
+stopTime=[]
+for i in range(4):
+    startTime.append(time.clock())
+    stopTime.append(None)
 
+faceTracking=True
 for cl in myList:
     curImg = cv2.imread(f"{path}/{cl}")
     images.append(curImg)
@@ -153,10 +95,11 @@ print('Encoding Complete')
 # cap = cv2.VideoCapture(0)
 class VideoCamera(object):
     def __init__(self):
-        #os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
+        os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"
         #self.cap = cv2.VideoCapture('rtsp://admin:888888@192.168.2.5:10554/udp/av0_0',cv2.CAP_FFMPEG)
-        self.cap = cv2.VideoCapture(0)
-
+        self.cap = cv2.VideoCapture('http://192.168.2.10:21866/videostream.cgi?user=admin&pwd=gigirivas',cv2.CAP_FFMPEG)
+        #self.cap = cv2.VideoCapture(0)
+        time.sleep(2)
         # success, image = self.cap.read()
 
     def __del__(self):
@@ -204,7 +147,7 @@ class VideoCamera(object):
                           (colorX, colorY, colorZ), 2)
             cv2.rectangle(img, (left - offset, bottom + offset), (right + offset, bottom), (colorX, colorY, colorZ),
                           cv2.FILLED)
-
+        
             if name != 'Unknown':
                 cv2.putText(img, name[0], (left - offset, bottom + offset - 25), cv2.FONT_HERSHEY_COMPLEX, 0.5,
                             (255, 255, 255), 2)
@@ -216,7 +159,33 @@ class VideoCamera(object):
                 now = datetime.now()
                 return_value, image = self.cap.read()
                 cv2.imwrite(f'/home/pi/Desktop/Tornado-mjpeg-streamer-python-master/Unknown-Faces/{now}.jpg', image)
-
+            if faceTracking:
+                y=bottom + offset - 25
+                x=left - offset
+                print(f"bottom:{bottom} left{left} x: {x}  y:{y}")
+                if x>360:
+                    stopTime[0]=time.clock()
+                    if stopTime[0] - startTime[0]>2:
+                        Right=requests.get('http://192.168.2.2:29626/decoder_control.cgi?loginuse=admin&loginpas=gigirivas&onestep=1&command=6') 
+                        startTime[0]=time.clock()
+                       
+                elif x<100:
+                    stopTime[1]=time.clock()
+                    if stopTime[1] - startTime[1]>2:
+                        Left=requests.get('http://192.168.2.2:29626/decoder_control.cgi?loginuse=admin&loginpas=gigirivas&onestep=1&command=4')
+                        startTime[1]=time.clock()
+                    
+                if y>330:
+                    stopTime[2]=time.clock()
+                    if stopTime[2] - startTime[2]>2:
+                        Down=requests.get('http://192.168.2.2:29626/decoder_control.cgi?loginuse=admin&loginpas=gigirivas&onestep=1&command=2')
+                        startTime[2]=time.clock()
+                elif y<170:
+                    stopTime[3]=time.clock()
+                    if stopTime[3] - startTime[3]>2:
+                        Up=requests.get('http://192.168.2.2:29626/decoder_control.cgi?loginuse=admin&loginpas=gigirivas&onestep=1&command=0')                           
+                        startTime[3]=time.clock()
+            
         cv2.imshow('Face Recognition', img)
         cv2.waitKey(1)  # 1: show video 0:show frame with a press o
 
@@ -227,9 +196,5 @@ class VideoCamera(object):
         while True:
             self.FaceRecognition()
 
-obj=VideoCamera()
-obj1=VideoCamera()
-
-'''while True:
-    obj.FaceRecognition()
-    obj1.FaceRecognition()'''
+'''obj=VideoCamera()
+obj.FaceRecognitionLoop()'''
--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template, Response, request,redirect,url_for,send_file
+from flask import Flask, render_template, Response, request,redirect,url_for
 import cv2
 from AttedanceProject import VideoCamera
-
+import requests
 app = Flask(__name__)
 
+#camera = cv2.VideoCapture('http://192.168.2.10:21866/videostream.cgi?user=admin&pwd=gigirivas',cv2.CAP_FFMPEG)
+        
 #camera = cv2.VideoCapture(0)  # use 0 for web camera
-#  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
+#for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
 # for local webcam use cv2.VideoCapture(0)
 
+app.route('/Download')
+def download_file ():
+    #For windows you need to use drive name [ex: F:/Example.pdf]
+    path = "home/pi/Desktop/FaceRecognitionProject/Attendance.csv"
+    return send_file(path, as_attachment=True)
 
 def gen_frames():  # generate frame by frame from camera
     CameraObj = VideoCamera()
@@ -24,32 +31,28 @@ def video_feed():
 
 @app.route('/buttonUP')
 def up():
+    Up=requests.get('http://192.168.2.2:29626/decoder_control.cgi?loginuse=admin&loginpas=gigirivas&onestep=1&command=0')  
     print("UP PRESSED")
+    print(Up.status_code, Up.reason)
     return (''),204
 @app.route('/buttonDOWN')
 def down():
+    Down=requests.get('http://192.168.2.2:29626/decoder_control.cgi?loginuse=admin&loginpas=gigirivas&onestep=1&command=2')
     print("DOWN PRESSED")
+    print(Down.status_code, Down.reason)
     return (''),204
 @app.route('/buttonLEFT')
 def left():
+    Left=requests.get('http://192.168.2.2:29626/decoder_control.cgi?loginuse=admin&loginpas=gigirivas&onestep=1&command=4')
     print("LEFT PRESSED")
+    
     return (''),204
 @app.route('/buttonRIGHT')
 def right():
+    Right=requests.get('http://192.168.2.2:29626/decoder_control.cgi?loginuse=admin&loginpas=gigirivas&onestep=1&command=6') 
     print("RIGHT PRESSED")
+    
     return (''),204
-
-@app.route('/button')
-def picture():
-    print("Picture Taken")
-    return (''),204
-
-@app.route('/return-file/')
-def return_file():
-    print("file downloaded")
-    return send_file('C:\\Users\\ddimitrakopoulos\\Documents\\MEGA\\Python\\FaceRecognition-Streaming\\Face recognition V1\\FaceRecognitionProject V1\\Attendance.csv')
-
-
 # @app.route('/index',methods=['GET', 'POST'])
 # def index():
 #     if request.method == 'POST':
@@ -79,4 +82,4 @@ def login():
 
 if __name__ == '__main__':
 
-    app.run(debug=False,threaded=True)
+    app.run(host='0.0.0.0',port=5000,debug=True,threaded=True)
